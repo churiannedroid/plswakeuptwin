265f0c56c78fc21933cc8aaa82495f077a6c34a9
churianne <aleinir16@gmail.com>
2026-09-11 19:11:19 +0800

    Update MainActivity.kt

--- app/src/main/java/com/raisetowake/app/MainActivity.kt
+++ app/src/main/java/com/raisetowake/app/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.SeekBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.raisetowake.app.R
 
 class MainActivity : AppCompatActivity() {
 
