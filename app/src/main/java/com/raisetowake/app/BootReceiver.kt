62980c045ef54fa6519f8e54acb460c8905759dd
churianne <aleinir16@gmail.com>
2026-09-11 19:55:10 +0800

    Update BootReceiver.kt

--- app/src/main/java/com/raisetowake/app/BootReceiver.kt
+++ app/src/main/java/com/raisetowake/app/BootReceiver.kt
@@ -3,18 +3,14 @@ package com.raisetowake.app
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.os.Build
+import androidx.core.content.ContextCompat
 
 class BootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
             if (Prefs.isServiceEnabled(context)) {
                 val serviceIntent = Intent(context, WakeService::class.java)
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    context.startForegroundService(serviceIntent)
-                } else {
-                    context.startService(serviceIntent)
-                }
+                ContextCompat.startForegroundService(context, serviceIntent)
             }
         }
     }
