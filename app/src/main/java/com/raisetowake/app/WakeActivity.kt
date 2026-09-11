ed0455b6914c9fbb938319fd7644e5a49d7e30f2
churianne <aleinir16@gmail.com>
2026-09-11 16:55:17 +0800

    Create WakeActivity.kt

---
+++ app/src/main/java/com/raisetowake/app/WakeActivity.kt
@@ -0,0 +1,24 @@
+package com.raisetowake.app
+
+import android.os.Build
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+class WakeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+            setShowWhenLocked(true)
+            setTurnScreenOn(true)
+        } else {
+            @Suppress("DEPRECATION")
+            window.addFlags(
+                WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or
+                WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+            )
+        }
+        finish()
+    }
+}
