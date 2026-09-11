86e6225ae3d84b593647a2a28f9e5746f151a684
churianne <aleinir16@gmail.com>
2026-09-11 17:17:05 +0800

    Update WakeActivity.kt

--- app/src/main/java/com/raisetowake/app/WakeActivity.kt
+++ app/src/main/java/com/raisetowake/app/WakeActivity.kt
@@ -1,24 +1,31 @@
 package com.raisetowake.app
 
-import android.app.Activity
+import android.app.KeyguardManager
+import android.content.Context
 import android.os.Build
 import android.os.Bundle
 import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 
-class WakeActivity : Activity() {
+class WakeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
             setShowWhenLocked(true)
             setTurnScreenOn(true)
+            val keyguardManager = getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+            keyguardManager.requestDismissKeyguard(this, null)
         } else {
             @Suppress("DEPRECATION")
             window.addFlags(
                 WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or
-                WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+                WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD or
+                WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON or
+                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
             )
         }
+
         finish()
     }
 }
