652fc1cddabcc8b8a800eda77f9400478ea0c293
churianne <aleinir16@gmail.com>
2026-09-11 19:37:10 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,8 +5,6 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-
-@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -14,6 +12,5 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
-
 rootProject.name = "RaiseToWake"
 include(":app")
